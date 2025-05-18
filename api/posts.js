f7c9d6ba253a8c684cb1a2af766e407c87b55b2c
GenerativeAI-openai <ijunu3343@gmail.com>
2025-05-18 16:29:03 +0900

    Update posts.js

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,7 @@
 import admin from "firebase-admin";
 import { getApps } from "firebase-admin/app";
 console.log(process.env.FIREBASE_PRIVATE_KEY);
-console.log(process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n").replace(/\//g, ""));
+console.log(process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n");
 // if (!getApps().length) {
 //   admin.initializeApp({
 //     credential: admin.credential.cert({
@@ -15,7 +15,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n").replace(/\//g, ""),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),//.replace(/\//g, ""),
   }),
 });
 const db = admin.firestore();
