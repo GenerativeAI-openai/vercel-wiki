2bd3397d6be61e46ab1c2d983c6ae8d27c1e40b3
GenerativeAI-openai <ijunu3343@gmail.com>
2025-06-01 16:05:13 +0900

    Update main.js

--- public/main.js
+++ public/main.js
@@ -221,16 +221,20 @@ document.addEventListener("DOMContentLoaded", async () => {
   const googleBtn = document.getElementById("googleLogin");
   if (googleBtn) {
     googleBtn.addEventListener("click", () => {
-      const auth = getAuth();
-      const provider = new GoogleAuthProvider();
-      signInWithRedirect(auth, provider)
-        .then(result => {
-          console.log("로그인 성공:", result.user);
-        })
-        .catch(error => {
-          console.error("로그인 실패:", error);
-        });
-      // loginAndLoad()
+      // const auth = getAuth();
+      // const provider = new GoogleAuthProvider();
+      // signInWithRedirect(auth, provider)
+      //   .then(result => {
+      //     console.log("로그인 성공:", result.user);
+      //   })
+      //   .catch(error => {
+      //     console.error("로그인 실패:", error);
+      //   });
+    signInWithPopup(auth, provider).then(async (result) => {
+      currentUser = result.user;
+      currentToken = await currentUser.getIdToken();
+      await loadPosts();
+    });
     });
   }
 });
