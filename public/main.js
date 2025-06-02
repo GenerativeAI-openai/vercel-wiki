3a2d75d2ac11010a5b6ce3015920dd6b81f29986
GenerativeAI-openai <ijunu3343@gmail.com>
2025-06-02 11:18:20 +0900

    Update main.js

--- public/main.js
+++ public/main.js
@@ -41,7 +41,13 @@ import {
 
 const firebaseConfig = {
   apiKey: "AIzaSyDzWZtDMzb4cNoauLA4NvmxqO6C_ot00AA",
-  authDomain: "smssend-b82b3.firebaseapp.com",//"smssend-b82b3.firebaseapp.comauth.wiki.kkomaweb.com"
+  authDomain: "smssend-b82b3.firebaseapp.com",
+  databaseURL: "https://smssend-b82b3-default-rtdb.firebaseio.com",
+  projectId: "smssend-b82b3",
+  storageBucket: "smssend-b82b3.firebasestorage.app",
+  messagingSenderId: "734888172114",
+  appId: "1:734888172114:web:0b2c99fee212f6487c1d8b",
+  measurementId: "G-J3VJPW92L2"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
